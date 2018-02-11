99f9c3ef081af46e31a6c02a6e3d02dd0b7fbe45
tombiasz <tomasz.m.nowacki@gmail.com>
2018-02-11 17:03:25 +0100

    add point of interest model

--- poi/models.py
+++ poi/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class PointOfInterest(models.Model):
+
+    name = models.CharField(max_length=255)
+    point = models.PointField(srid=4326)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'point of interest'
+        verbose_name_plural = 'points of interest'
