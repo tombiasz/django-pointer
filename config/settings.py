62afa5282cfd0f9415e79b9ab1614c2734f3d8e3
tombiasz <tomasz.m.nowacki@gmail.com>
2018-02-11 17:02:01 +0100

    use spatialite as db backend

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
 
 
 # Password validation
